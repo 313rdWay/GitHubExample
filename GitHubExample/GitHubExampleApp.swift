8f11e9fc895cf7f218f891c02c577750015decfc
Davaughn Williams <313rdWay@gmail.com>
2024-12-11 14:36:00 -0500

    Initial Commit

---
+++ GitHubExample/GitHubExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExampleApp.swift
+//  GitHubExample
+//
+//  Created by Davaughn Williams on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
